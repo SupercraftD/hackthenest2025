d1ac990737ab2b388fa0bd26566b3843362cf051
SupercraftD <dzhu700@gmail.com>
2025-09-20 13:47:21 -0400

    pleaase

--- interactables/Interactable.gd
+++ interactables/Interactable.gd
@@ -1,19 +1,17 @@
 class_name Interactable extends Area2D
 
+func _ready():
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		print("Player can interact with chest!")
-		# Add a prompt, like an exclamation mark, over the player's head.
+		body.interacting.append(self)
+		
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
-		print("Player exited interaction area.")
-		# Hide the prompt.
+		body.interacting.erase(self)
 
-
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("interact") and overlaps_body(get_node_or_null("/root/MainScene/Player")):
-		# Get a reference to your player node.
-		# Check if the player is currently overlapping the Area2D.
-		print("FOlder interacted with!")
-		# Add your interaction code here (e.g., play animation, open menu).
+func interact(player):
+	pass
--- player.gd
+++ player.gd
@@ -3,12 +3,45 @@ extends CharacterBody2D
 @onready var roll = $Roll
 @export var cursorProj : PackedScene
 
+var inDialogue = false
+var interacting = []
+
 @onready var dustParticles = $dust
 var speed = 150
 
 var canShoot = true
 
+func _ready():
+	promptDialogue(["hiqhiqhiqjewifje","ofjwiwjiowejiwhe"])
+
+func promptDialogue(tree):
+	inDialogue = true
+	await $CanvasLayer/dialoguepopup.showDialogue(tree)
+	inDialogue = false
+
 func _physics_process(delta):
+	
+	move()
+	
+	if (velocity == Vector2.ZERO):
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.play("run")
+	
+	if len(interacting)>0 and not inDialogue:
+		$CanvasLayer/interactlabel.visible = true
+	else:
+		$CanvasLayer/interactlabel.visible = false
+	
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+	
+	if Input.is_action_just_pressed("interact"):
+		interact()
+
+func move():
+	if inDialogue:
+		return
 	
 	if Input.is_action_just_pressed("roll"):
 		roll.startRoll(2)
@@ -19,16 +52,14 @@ func _physics_process(delta):
 	
 	velocity = Input.get_vector("left","right","up","down") * speed
 	move_and_slide()
+
+
+func interact():
+	if inDialogue:
+		return
 	
-	if (velocity == Vector2.ZERO):
-		$AnimatedSprite2D.play("idle")
-	else:
-		$AnimatedSprite2D.play("run")
-	
-	
-	
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+	for i in interacting:
+		i.interact(self)
 
 
 func shoot():
