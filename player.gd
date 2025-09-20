5170caee8b50474397a4936e4b7e17412e9b54fa
SupercraftD <dzhu700@gmail.com>
2025-09-20 11:43:00 -0400

    player anims

--- player.gd
+++ player.gd
@@ -5,7 +5,9 @@ var speed = 400
 func _physics_process(delta):
 	
 	velocity = Input.get_vector("left","right","up","down") * speed
-	
 	move_and_slide()
 	
-	
+	if (velocity == Vector2.ZERO):
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.play("run")
