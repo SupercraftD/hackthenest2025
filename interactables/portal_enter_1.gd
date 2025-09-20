8fc1c372e1c2fe23801b843b20d114e7c6c7e637
RanchDressin1211 <reyanshmd@gmail.com>
2025-09-20 19:18:31 -0400

    Update portal_enter_1.gd

--- interactables/portal_enter_1.gd
+++ interactables/portal_enter_1.gd
@@ -3,3 +3,4 @@ func interact(player):
 	player.promptDialogue([
 		"The portal seems to be a way out. It is the only way to move forward."
 		])
+	get_tree().change_scene_to_file("res://firewallfight/firearena.tscn")
