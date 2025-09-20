eab4ed457608887a47ce9b60f6bf64689db027b6
RanchDressin1211 <reyanshmd@gmail.com>
2025-09-20 14:23:25 -0400

    Added first lore drop

---
+++ interactables/lore_1.gd
@@ -0,0 +1,4 @@
+extends Interactable
+
+func interact(player):
+	player.promptDialogue(["You could not resist your urges could you? Even though we warned you.", "Some parts of the internet are too dangerous for you to visit.", "You are currently detached from the cyberspace- past the firewall perimeter.", "This is for you own good."])
