7f6350ddf2d9a81e038b1429d1eec0fdd6a75b9a
RanchDressin1211 <reyanshmd@gmail.com>
2025-09-20 22:00:17 -0400

    world built and lore

--- firewallfight/fire_wall.gd
+++ firewallfight/fire_wall.gd
@@ -53,7 +53,7 @@ func _physics_process(delta: float) -> void:
 func die():
 	done = true
 	emit_signal("dead")
-	await player.promptDialogue(["[b]Firewall: [/b]Symtems... overloaded... Critical damage recieved... warning TSP... Good luck, user.","You have unlocked the FLAME THROWER. Press F to use!"])
+	await player.promptDialogue(["[b]Firewall: [/b]Symtems... overloaded... Critical damage recieved... Sending distress call... Good luck, user. ","You'll never get past [b] him. [/b]","You have unlocked the FLAME THROWER. Press F to use!"])
 	GameInfo.availableWeapons.append("flamethrower")
 	queue_free()
 
---
+++ interactables/lore_2.gd
@@ -0,0 +1,4 @@
+extends Interactable
+
+func interact(player):
+	player.promptDialogue(["You breached the fire wall. Impressive.","You really want to reach that website don't you?", "You may be determined, but my protocol is to protect this device by all means. Even against [b]you.[/b]", "And so, I've sent my most trusted ally to stop you. The DNS", "He holds the adress to every website on the Internet. He is all-knowing. And he will never share his secrets.","Be advised as he brings to life lost IPs and corrupted worms","This is for the greater good.", "-The Sentinel Protocol (TSP)"])
