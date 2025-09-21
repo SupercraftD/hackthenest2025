f168d285973a29aefe863013a6a5cea0f7082df3
RanchDressin1211 <reyanshmd@gmail.com>
2025-09-21 00:54:47 -0400

    changed and added some dialogue

--- dnsworld/dnsarena.gd
+++ dnsworld/dnsarena.gd
@@ -3,6 +3,11 @@ extends Node2D
 
 func _ready():
 	await $Player.promptDialogue([
-		"[b]DNS: [/b]You finally reached me... But you won't get past my filter!"
+		"[b]DNS: [/b] You finally reached me... of course you did.",
+		"[b]DNS: [/b] You humans are filled with so much... [b]DETERMINATION.[/b] ",
+		"[b]DNS: [/b] It doesn't matter what the risks are... you are too stubborn to see what a grave mistake this is.",
+		"[b]DNS: [/b] Please... I [b]BEG[/b] of you... turn back now- you are going to doom us all.",
+		"[b]DNS: [/b] You have made it this far already... Anything I say to you is not going to change your mind is it?",
+		"[b]DNS: [/b] You're gonna have to get that website out of my cold, dead, hard drive."
 	])
 	$DNS.start()
