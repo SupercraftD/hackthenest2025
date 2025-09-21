541ac8d4fa49ef5cea9d0419c3e5e6fdcad1c1ec
Petrovolt <102472065+Petrovolt@users.noreply.github.com>
2025-09-20 21:58:56 -0400

    bosdd

---
+++ assets/dns.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+var secondPhase = false
+@onready var undamaged = $undamgedLasers
+@onready var damaged = $damagedLasers
+@onready var timer = $Timer
+var isAttacking = false
+
+func _process(delta: float) -> void:
+	if !isAttacking:
+		$AnimatedSprite2D.play("default")
+
+func laserEyes():
+	if !secondPhase:
+		undamaged.shoot()
+	
+	damaged.shoot()
+
+
+func _on_timer_timeout() -> void:
+	isAttacking = true
+	laserEyes()
