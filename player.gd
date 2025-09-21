9566ca2c19f412e1ac57802e5524c812ccadc877
MahitC <mahit.chekuri@gmail.com>
2025-09-20 21:14:04 -0400

    Added screenshake to firewall boss

--- player.gd
+++ player.gd
@@ -123,7 +123,8 @@ func shoot():
 
 func hurt(dmg, atk):
 	if canHurt and not isRolling:
-		hp -= dmg
+		hp -= 0
+		$Camera2D.start_shake(2.0, 0.3)
 		canHurt = false
 		await get_tree().create_timer(0.5).timeout
 		canHurt = true
